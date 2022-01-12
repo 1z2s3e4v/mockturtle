75739c11e690c0d56b54eede4f684c3c13341a1b
JoshuaLin0903 <b06901016@ntu.edu.tw>
2022-01-12 16:22:22 +0800

    add epfl_small benchmark option

--- experiments/window_rewriting.cpp
+++ experiments/window_rewriting.cpp
@@ -56,7 +56,7 @@ int main()
   experiment<std::string, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, uint64_t, double, bool>
     exp( "window_rewriting", "benchmark", "size_before", "size_after", "est. gain", "real gain", "resubs", "iterations", "runtime", "equivalent" );
 
-  for ( auto const& benchmark : all_benchmarks( iscas | epfl ) )
+  for ( auto const& benchmark : all_benchmarks( iscas | epfl_small ) )
   {
     fmt::print( "[i] processing {}\n", benchmark );
 
